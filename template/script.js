0ce5b13434d282dbf48471813ef833fde5b577b4
d3mola <ariyaademola@gmail.com>
2017-11-15 10:29:01 +0100

    Add script to toggle edit button in modify-event.html

---
+++ template/script.js
@@ -0,0 +1,25 @@
+$(document).ready(() => {
+  let notdone = true;
+  $('#btn').click(() => {
+    if (notdone) {
+      $('.title').prop('disabled', false);
+      $('#btn').val('Done');
+      // $('#btn').html('Done');
+      $('#btn').html('<i class="fa fa-edit fa-lg fw"></i> Submit');
+      notdone = false;
+      $('#btn').click(() => {
+        $('.title').prop('disabled', true);
+        
+      });
+    } else {
+      $('.title').prop('disabled', true);
+      $('#btn').val('Edit');
+      $('#btn').html('<i class="fa fa-edit fa-lg fw"></i> Edit');
+      notdone = true;
+      $('#btn').click(() => {
+        $('.title').prop('disabled', false);
+        
+      });
+    }
+  });
+});
