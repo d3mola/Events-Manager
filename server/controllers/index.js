703ee6d1a100e5a10e804fb520ae7174fc0e196e
d3mola <ariyaademola@gmail.com>
2017-11-18 22:52:54 +0100

    Add file to store all controllers

---
+++ server/controllers/index.js
@@ -0,0 +1,5 @@
+import userController from './userController';
+// import eventController from './eventController';
+// import centerController from './centerController';
+
+export default { userController };
