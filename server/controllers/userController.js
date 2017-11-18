6b7cd3d0d15c2eea1b9d321b93784f98d7227adb
d3mola <ariyaademola@gmail.com>
2017-11-18 22:50:28 +0100

    Add controller function to create a new user i.e signup

---
+++ server/controllers/userController.js
@@ -0,0 +1,18 @@
+import db from '../models';
+
+const User = db.User;
+
+export default {
+
+  create(req, res) {
+    return User
+      .create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+        isAdmin: req.body.isAdmin
+      })
+      .then(user => res.status(201).send(user))
+      .catch(error => res.status(400).send(error.toString()));
+  },
+};
