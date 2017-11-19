956e3d62dcf96e79774dac551184154237984ed7
d3mola <ariyaademola@gmail.com>
2017-11-19 12:44:16 +0100

    Check if a user that wants to register already exists

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -5,15 +5,33 @@ const { User } = db;
 export default {
 
   signup(req, res) {
-    return User
-      .create({
+    // check if username/ email are already taken
+    return User.find({
+      where: {
+        $or: [{ username: req.body.username }, { email: req.body.email }]
+      }
+    }).then((existingUser) => {
+      if (existingUser && existingUser.username === req.body.username) {
+        return res.status(400).send('Username taken!');
+      }
+      if (existingUser && existingUser.email === req.body.email) {
+        return res.status(400).send('Another account uses this email!');
+      }
+      User.create({
         username: req.body.username,
         email: req.body.email,
         password: req.body.password,
         isAdmin: req.body.isAdmin
       })
-      .then(user => res.status(201).send(user))
-      .catch(error => res.status(400).send(error.toString()));
+        .then(user => res.status(201).send({
+          status: 'success',
+          message: 'Congrats!!! Registration succesfully!',
+          user
+        }))
+        .catch(error => res.status(400).send(error.toString()));
+    }).catch(error => res.status(500).send(error).toString());
+
+    /*  */
   },
 
   login(req, res) {
