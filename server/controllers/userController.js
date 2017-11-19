38ec5545b71504ab0e0a90e68f3721c1f05f9561
d3mola <ariyaademola@gmail.com>
2017-11-19 13:48:31 +0100

    Validate password during signup

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -5,7 +5,16 @@ const { User } = db;
 export default {
 
   signup(req, res) {
-    // check if username/ email are already taken
+    // check for empty password.. Add reqex to validate password later
+    if (!req.body.password) {
+      return res.status(400).send('Enter a password! (`_`)/```');
+    }
+    // check password length
+    if (req.body.password.length < 6) {
+      return res.status(400).send('Password should be atleast 6 characters long!');
+    }
+
+    // check if username/ email are already taken, then return error messages
     return User.find({
       where: {
         $or: [{ username: req.body.username }, { email: req.body.email }]
@@ -17,6 +26,7 @@ export default {
       if (existingUser && existingUser.email === req.body.email) {
         return res.status(400).send('Another account uses this email!');
       }
+      // if username/ password arent already taken, create the user
       User.create({
         username: req.body.username,
         email: req.body.email,
@@ -35,11 +45,15 @@ export default {
   },
 
   login(req, res) {
+    // check for empty username or password
+    if (!req.body.username/* || !req.body.password */) {
+      return res.status(400).send('Incomplete credentials');
+    }
     return User
       .findOne({
         where: {
           username: req.body.username,
-          password: req.body.password
+          // password: req.body.password
         }
       })
       .then(user => res.status(201).send(user))
