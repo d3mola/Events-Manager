bff82f26b6a0d10efa639ab9103729b9096d8171
d3mola <ariyaademola@gmail.com>
2017-11-20 20:39:46 +0100

    Import and use authenticate and adminCheck middlewares

--- server/routes/centers.js
+++ server/routes/centers.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import centerController from '../controllers/centerController';
+import authenticate from '../middleware/authenticate';
+import adminCheck from '../middleware/adminCheck';
 
 const router = express.Router();
 
-router.post('/centers', centerController.create);
+router.post('/centers', authenticate, adminCheck, centerController.create);
 
 export default router;
