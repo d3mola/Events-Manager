e6361dd6b0311b94ed3cd7cdddaaaea3b75107ec
d3mola <ariyaademola@gmail.com>
2017-11-21 23:59:59 +0100

    Add route to create an event

---
+++ server/routes/events.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import eventController from '../controllers/eventController';
+import authenticate from '../middleware/authenticate';
+
+const router = express.Router();
+
+router.post('/events', authenticate, eventController.createEvent);
+
+// router.put('/events/:eventId', authenticate, eventController.updateEvent);
+
+// router.delete('/events/:eventId', authenticate, eventController.deleteEvent);
+
+export default router;
