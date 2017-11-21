18000015b97a23609235a6f243e249f3e1a56855
d3mola <ariyaademola@gmail.com>
2017-11-21 23:56:31 +0100

    Swapped order of center and event migration files

---
+++ server/migrations/20171118103811-create-event.js
@@ -0,0 +1,47 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Events', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    title: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    notes: {
+      type: Sequelize.TEXT
+    },
+    date: {
+      type: Sequelize.DATEONLY,
+      allowNull: false
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    userId: {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'Users',
+        key: 'id',
+        as: 'userId',
+      },
+    },
+    centerId: {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'Centers',
+        key: 'id',
+        as: 'centerId',
+      },
+    },
+  }),
+
+  down: queryInterface => queryInterface.dropTable('Events')
+};
