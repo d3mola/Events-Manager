f044b741e34ab86586e17d09166fabe790e136e6
d3mola <ariyaademola@gmail.com>
2017-11-21 06:17:52 +0100

    Add controller logic to update a center

--- server/controllers/centerController.js
+++ server/controllers/centerController.js
@@ -3,6 +3,12 @@ import db from '../models';
 const { Center } = db;
 export default {
 
+  /**
+   * @description adds a new center to the database
+   * @param {object} req HTTP request object
+   * @param {object} res HTTP response object
+   * @returns {object} new center
+   */
   create: (req, res) => {
     const {
       name, location, capacity, price, isAvailable
@@ -25,5 +31,33 @@ export default {
         message: 'Something went wrong, unable to create center',
         error
       }));
-  }// end of centerController.create
-};// end of export default
+  }, // end of centerController.create
+
+
+  /**
+   * @description updates Center information
+   * @param {object} req HTTP request object
+   * @param {object} res HTTP response object
+   * @returns {object} updated center
+   */
+  update: (req, res) => {
+    if (req.user && req.user.isAdmin) {
+      return Center
+        .update(req.body, { where: { id: req.params.centerId } })
+        .then(center => res.status(200).json({
+          success: true,
+          message: 'Center updated succesfully',
+          center
+        }))
+        .catch(error => res.status(500).json({
+          success: false,
+          message: 'Something went wrong',
+          error
+        }));
+    }
+    res.status(401).json({
+      success: false,
+      message: 'Unauthohrized access'
+    });
+  }
+};
