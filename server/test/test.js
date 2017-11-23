d3eb72d2bbd1520bfa5b11120be10d6492e4b6a1
d3mola <ariyaademola@gmail.com>
2017-11-23 09:06:03 +0100

    Add tests to check login api

--- server/test/test.js
+++ server/test/test.js
@@ -8,16 +8,9 @@ import db from '../models';
 dotenv.config();
 const { User } = db;
 const request = supertest(app);
-// let data = {};
-// const url = '/api/v1';
+
+
 describe('User', () => {
-  // let regularToken, adminToken;
-  /* beforeEach((done) => {
-    db.User.destroy({
-      where: {}
-    });
-    done();
-  }); */
   beforeEach((done) => {
     db.sequelize.sync({ force: true }).then(() => {
       done(null);
@@ -31,6 +24,15 @@ describe('User', () => {
       email: 'ademola@gmail.com',
       password: 'password1',
       isAdmin: true
+    });
+    done();
+  });
+  beforeEach((done) => {
+    User.create({
+      username: 'babajide',
+      email: 'babajide@gmail.com',
+      password: 'babajide',
+      isAdmin: false
     });
     done();
   });
@@ -124,4 +126,75 @@ describe('User', () => {
         });
     });
   }); // end of POST rreq
+
+  describe('POST /api/users/login', () => {
+    it('return 200 when admin login is succesful', (done) => {
+      request.post('/api/v1/users/login')
+        .send({
+          username: 'ademola',
+          password: 'password1'
+        })
+        .end((err, res) => {
+          expect(res.status).to.equal(200);
+          expect(res.body.success).to.equal(true);
+          done();
+        });
+    });
+
+    it('return 403 if user doesnt exist', (done) => {
+      request.post('/api/v1/users/login')
+        .send({
+          username: 'kjbsafld/jm',
+          password: 'password1'
+        })
+        .end((err, res) => {
+          expect(res.status).to.equal(403);
+          expect(res.body.success).to.equal(false);
+          expect(res.body.message).to.equal('Incorrect username');
+          done();
+        });
+    });
+
+    it('return 403 if passwords dont match', (done) => {
+      request.post('/api/v1/users/login')
+        .send({
+          username: 'ademola',
+          password: 'password2'
+        })
+        .end((err, res) => {
+          expect(res.status).to.equal(403);
+          expect(res.body.success).to.equal(false);
+          expect(res.body.message).to.equal('Incorrect password');
+          done();
+        });
+    });
+
+
+    it('return 500 for incomplete credentials', (done) => {
+      request.post('/api/v1/users/login')
+        .send({
+          username: 'ademola'
+        })
+        .end((err, res) => {
+          expect(res.status).to.equal(500);
+          expect(res.body.success).to.equal(false);
+          expect(res.body.message).to.equal('Incomplete credentials');
+          done();
+        });
+    });
+
+    it('return 200 for successful login', (done) => {
+      request.post('/api/v1/users/login')
+        .send({
+          username: 'ademola',
+          password: 'password1'
+        })
+        .end((err, res) => {
+          expect(res.status).to.equal(200);
+          expect(res.body.success).to.equal(true);
+          expect(res.body.message).to.equal('Enjoy your token! ademola');
+          done();
+        });
+    });
+  }); // end of login
 }); // end of USER API
