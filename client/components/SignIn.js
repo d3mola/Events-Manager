9b3d89ad949c62c36961ac4a61b897521ae7ec3b
d3mola <ariyaademola@gmail.com>
2017-11-29 17:42:42 +0100

    Add signin component

---
+++ client/components/SignIn.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import * as actionCreators from '../actions/actionCreators';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../../template/stylesheet/style.css';
+
+/**
+* @description Creates SignIn component
+* @class SignIn
+* @extends {React.Component}
+*/
+class SignIn extends React.Component {
+  /**
+   * Creates an instance of SignIn.
+   * @memberof SignIn
+   */
+  constructor(props) {
+    super(props);
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+
+    //setting the initial state of the component
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }// constructor
+
+  /**
+  * submit handler
+  * @param {any} event
+  * @memberof SignIn
+  * @returns {}
+  */
+
+  /**
+  * sign up handler
+  * @param {any} event
+  * @memberof SignIn
+  * @returns {Undefined} sets form input values
+  */
+  handleChange(e) {
+    // set the next state of the form
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+    console.log(this.state)
+  }// handleSubmit
+
+  
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(this.state)
+    this.props.signIn(this.state);
+  };// handleSubmit
+
+/**
+ * renders signUpForm component
+ * @returns rendered component
+ * @memberof SignIn
+ */
+ render() {
+   return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 mx-auto">
+            <form id="form-box" className="jumbotron" action="" method="post" onSubmit={this.handleSubmit}>
+              <h2 className="text-center">Sign In!</h2>
+
+              <div className="form-group">
+                  <label htmlFor="email">Email:</label>
+                  <input className="form-control" type="email" name="email" id="email" placeholder="johndoe@email.com" onChange={this.handleChange} />
+              </div>
+
+              <div className="form-group">
+                  <label htmlFor="password">Password:</label>
+                  <input className="form-control" type="password" name="password" id="password" placeholder="Enter your password" onChange={this.handleChange} />                  
+              </div>
+              <input className="btn btn-outline-success" type="submit" value="Sign in" />
+              <br />
+              <p className="text-center">Don't have an account yet? <Link href to={"/signup"}>Register</Link></p>
+            </form>
+        </div>
+      </div>
+    </div>
+
+   );
+ }
+};
+
+function mapStateToProps(state) {
+  return {
+    signinReducer: state.SignIn,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(actionCreators, dispatch);
+}
+
+const signinReducer = connect(mapStateToProps, mapDispatchToProps)(SignIn);
+
+export default signinReducer;
