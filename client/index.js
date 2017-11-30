e502979aa8923b7a5828644f8c8b3ecbdd2065ab
d3mola <ariyaademola@gmail.com>
2017-11-30 09:38:41 +0100

    Clean up code

--- client/index.js
+++ client/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
-import SignUp from './components/SignUp';
-
-
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store, { history } from './store';
 
+/**
+ * allows/provides for communication between the components and the store
+ */
 const router = (
   <Provider store={store}>
     <App />
