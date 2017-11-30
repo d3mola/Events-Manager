690611a702ccf05e2fbc52d1b27cc73f8335c36e
d3mola <ariyaademola@gmail.com>
2017-11-30 09:38:58 +0100

    Log the state to the console

--- client/store.js
+++ client/store.js
@@ -8,10 +8,13 @@ import rootSaga from './Saga/sagax';
 
 const sagaMiddleware = createSagaMiddleware();
 
+// create the store
 const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
 
 export const history = syncHistoryWithStore(createBrowserHistory(), store);
 
 sagaMiddleware.run(rootSaga);
+
+console.log(store.getState());// get current state of the store
 
 export default store;
