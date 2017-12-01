74a1066120bc533f4b6ace52f8238a7d4154584e
d3mola <ariyaademola@gmail.com>
2017-12-01 14:38:21 +0100

    Add route to centers page

--- client/components/App.js
+++ client/components/App.js
@@ -4,12 +4,18 @@ import history from '../history';
 import Home from './Home';
 import SignUp from './SignUp';
 import SignIn from './SignIn';
+import Centers from './Centers';
 
 /**
  * @description creates a component that takes in all the routes as children
- * @
+ * @returns {class} App
  */
 class App extends React.Component {
+  /**
+   * 
+   * @returns {component} router
+   * @memberof App
+   */
   render() {
     return (
       <Router history={history}>
@@ -17,6 +23,7 @@ class App extends React.Component {
           <Route exact path="/" component={Home} />
           <Route exact path="/signup" component={SignUp} />
           <Route exact path="/signin" component={SignIn} />
+          <Route exact path="/centers" component={Centers} />
         </Switch>
       </Router>
     )
