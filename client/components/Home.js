a6beba4a810cf36c04c3afdba9b46cdbf6acf6c6
d3mola <ariyaademola@gmail.com>
2017-12-02 09:12:06 +0100

    Heroku issue

--- client/components/Home.js
+++ client/components/Home.js
@@ -3,7 +3,8 @@ import { Link, Redirect } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 
-import '../static/scss/home.scss';
+// import '../static/scss/home.scss';
+import '../../template/stylesheet/index.css'
 
 /**
  * 
