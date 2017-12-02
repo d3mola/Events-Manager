b357851bca359ad3cd461ddaa465f86efec02c7f
d3mola <ariyaademola@gmail.com>
2017-12-02 02:16:39 +0100

    Render centers

--- client/components/Centers.js
+++ client/components/Centers.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link, Redirect } from 'react-router-dom';
-import * as actionCreators from '../actions/actionCreators';
-// import centers from '../actions/actionCreators';
-import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { centers } from '../actions/actionCreators';
+import Header from './SignupHeader';
+
+import '../../template/stylesheet/center-details.css';
+
 /**
- * 
  * 
  * @class Centers
  * @extends {React.Component}
@@ -14,11 +15,11 @@ class Centers extends React.Component {
   /**
    * Creates an instance of Centers.
    * @memberof Centers
+   * @param {object} props
    */
-  constructor() {
-    super();
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+  constructor(props) {
+    super(props);
+  }//
 
   /**
    *  get centers as soon as component renders
@@ -26,72 +27,88 @@ class Centers extends React.Component {
    * @memberof Centers
   */
   componentWillMount() {
-       this.props.centers();
+       this.props.actions();
       // console.log('fetched centers ==>', fetchedcenters);
   }
 
   /**
-   * 
-   * @returns {object}.
-   * @param {any} event
-   * @memberof Centers
-   */
-  handleSubmit(event) {
-      event.preventDefault();
-      console.log(this.state)
-      this.props.centers(this.state);
-    } 
-
-  /**
-   * 
+   * function where te login of our display is done so we
+   * can keep the main render function clean.
+   * we simply call the renderCenters in the render function
    * @returns {array} centers
    * @memberof Centers
    */
   renderCenters() {
-
     const allCenters = this.props.centers;
     console.log(allCenters);
-    return allCenters.map((center) => {
+    if (!this.props.centers) {
       return (
-        <div>
-          {center.name}
-        </div>
-      );
-    });
+        <p>Loading data...</p>
+      )
+    } else {
+      return allCenters.map(((center, index) => {
+        console.log(center['1'], index)
+        return (
+          <div key={center[index].id} className="col-12 col-md-6 text-center">
+            <div className="card">
+              <div className="card-body">
+                <h4 className="card-title">{center[index].name}</h4>
+                <p className="card-text">{center[index].location}</p>
+                <p className="card-text">{center[index].capacity}</p>
+                <p className="card-text">{'#'+center[index].price}</p><hr />
+                <a href="#" className="btn card-link"><i className="fa fa-edit fa-lg fw"></i> Update</a>
+              </div>
+            </div>
+          </div>
+            
+        )
+      }));
+    }
+    
   }
 
   /**
-   * 
+   * our main render function which calls the above renderCenters function
+   * and displays our data
    * @returns {object} component
    * @memberof Centers
    */
   render() {
-      return (
-        <div className="container">
-          <h1> i came hereeeeeeeeeeeee</h1>
-          { this.renderCenters() }
-        </div>
-  
-      );
-    }
+    return (
+      <div className="container">
+        <Header />
+        <section id="main">
+            <div className="container">
+              <div className="row fill-viewport">
+                { this.renderCenters() }
+              </div>
+            </div>
+        </section>
+          
+      </div>
+
+    );
   }
+}
   
 /**
  * @param {any} state 
  * @returns {object}. 
  */
-function mapStateToProps(state) {
+const mapStateToProps = (state => {
   return {
-    centers: state.centers || [],
+    centers: state.centers,
   };
-}
+});
 
 /**
  * @param {any} dispatch 
  * @returns {object}. 
  */
-function mapDispatchToProps(dispatch) {
-return bindActionCreators(actionCreators, dispatch);
-}
+const mapDispatchToProps = (dispatch => {
+  return {
+    actions: () => dispatch(centers())
+  };
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Centers);
