b246184bb73ade04d495ce97e1a9fc587eea1b60
d3mola <ariyaademola@gmail.com>
2017-12-24 15:38:44 +0100

    Return proper state for 'GET_CENTERS_SUCCESS' case in centers reducer

--- client/reducers/signinReducer.js
+++ client/reducers/signinReducer.js
@@ -8,7 +8,7 @@
 const SignIn = ((state = [], action) => {
   switch (action.type) {
     case 'SIGN_IN':
-      console.log('signing in');
+      console.log('signing in', action);
       return state;
     default:
       return state;
