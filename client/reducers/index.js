6e0060bd51b45bd8e14351ab3575b44b4abe02a7
d3mola <ariyaademola@gmail.com>
2018-01-11 03:08:38 +0100

    Add events reducer to combined reducres

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -4,7 +4,14 @@ import { routerReducer } from 'react-router-redux';
 import signUp from './userSignUpReducer';
 import signIn from './signinReducer';
 import centers from './centersReducer';
+import events from './eventsReducer';
 
-const rootReducer = combineReducers({ signUp, signIn, centers, routing:routerReducer });
+const rootReducer = combineReducers({
+  signUp,
+  signIn,
+  centers,
+  events,
+  routing: routerReducer
+});
 
 export default rootReducer;
