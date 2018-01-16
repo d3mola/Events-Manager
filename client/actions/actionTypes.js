2ef90f587a9986acdefacfdad01719763c12fcd1
d3mola <ariyaademola@gmail.com>
2018-01-16 16:58:59 +0100

    Add actionTypes to edit an event

--- client/actions/actionTypes.js
+++ client/actions/actionTypes.js
@@ -2,4 +2,11 @@ export const ADD_EVENT_LOADING = 'ADD_EVENT_LOADING';
 export const ADD_EVENT = 'ADD_EVENT';
 export const ADD_EVENT_SUCCESS = 'ADD_EVENT_SUCCESS';
 export const ADD_EVENT_ERROR = 'ADD_EVENT_ERROR';
+export const EDIT_EVENT = 'EDIT_EVENT';
+export const EDIT_EVENT_SUCCESS = 'EDIT_EVENT_SUCCESS';
+export const EDIT_EVENT_ERROR = 'EDIT_EVENT_ERROR';
+export const GET_EVENTS = 'GET_EVENTS';
+export const GET_EVENTS_SUCCESS = 'GET_EVENT_SUCCESS';
+// export const GET_EVENT = 'GET_EVENT';
+// export const DELETE_EVENT = 'DELETE_EVENT';
 
