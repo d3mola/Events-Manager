2e171a6429ac17a0be1f7b16a6ac758d1c703bda
d3mola <ariyaademola@gmail.com>
2018-01-30 12:15:22 +0100

    Rename events to eventsReducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -4,13 +4,13 @@ import { routerReducer } from 'react-router-redux';
 import signUp from './userSignUpReducer';
 import signIn from './signinReducer';
 import centers from './centersReducer';
-import events from './eventsReducer';
+import eventsReducer from './eventsReducer';
 
 const rootReducer = combineReducers({
   signUp,
   signIn,
   centers,
-  events,
+  eventsReducer,
   router: routerReducer
 });
 
