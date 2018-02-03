604e1ddee62a5fda20e74cb280a85f3dd9915ae3
d3mola <ariyaademola@gmail.com>
2018-02-03 14:08:58 +0100

    Allow toggle of event form component

--- client/components/EditEventForm.jsx
+++ client/components/EditEventForm.jsx
@@ -15,46 +15,47 @@ class EditEventForm extends Component {
   render() {
     const data = this.props.data;
     const { title, notes, center, date } = data;
-    const { handleChange, handleSubmit } = this.props;
-    return (
-      <div className="jumbotron">
-          <form id="form-box" action="" method="post" onSubmit={handleSubmit}>
-            <h3 className="text-center" style={{bottom:10}}>Edit Event!</h3>
-            
-            <div className="form-group">
-                <label htmlFor="event-name">Event Name:</label>
-                <input className="form-control" type="text" name="title" id="event-name" placeholder="E.g. John's convocation" value={title} onChange={handleChange} required/>                  
-            </div>
+    let { handleChange, handleSubmit } = this.props;
+    return (<div className={this.props.shouldHide ? 'hidden' : ''}>
+      <div className="jumbotron edit-event-form">
+        <form id="form-box" action="" method="post" onSubmit={handleSubmit}>
+          <h3 className="text-center" style={{ bottom: 20 }}>Edit Event!</h3>
+          <em style={{ fontWeight: 700, marginBottom: 20, color: 'black' }}>editing {title}...</em>
 
-            <div>
-              <label htmlFor="notes">Optional Note:</label>
-              <textarea className="form-control" name="notes" id="notes" cols="50" rows="4" placeholder="Enter an optional note"  value={notes} onChange={handleChange}></textarea>
-            </div>
-            <br />
+          <div className="form-group">
+            <label htmlFor="event-name">Event Name:</label>
+            <input className="form-control" type="text" name="title" id="event-name" placeholder="E.g. John's convocation" value={title} onChange={handleChange} required />
+          </div>
 
-            <div className="form-group">
-              <label htmlFor="center">Event Center: </label>
-              <select required className="form-control" name="center" id="center" value={center} onChange={handleChange}>
-                <option value="1" title="Oluyole, Ibadan">First hall</option>
-                <option value="2" title="Banana Island">Emerald hall</option>
-                <option value="3" title="Ketu">Sapphire</option>
-                <option value="4" title="Ikoyi">Gold</option>
-                <option value="5" title="Oluyole, Ibadan">Silver hall</option>
-                <option value="6" title="Victoria Island Lagos">Ruby Hall</option>
-              </select>
-            </div>
+          <div>
+            <label htmlFor="notes">Optional Note:</label>
+            <textarea className="form-control" name="notes" id="notes" cols="50" rows="4" placeholder="Enter an optional note" value={notes} onChange={handleChange}></textarea>
+          </div>
+          <br />
 
-            <div className="form-group">
-              <label htmlFor="date">Choose a date and time:<br />MM/DD/YYYY</label>
-              <input required className="form-control" type="date" name="date" id="date" value={date} onChange={handleChange}/>
-            </div>
+          <div className="form-group">
+            <label htmlFor="center">Event Center: </label>
+            <select required className="form-control" name="center" id="center" value={center} onChange={handleChange}>
+              <option value="1" title="Oluyole, Ibadan">First hall</option>
+              <option value="2" title="Banana Island">Emerald hall</option>
+              <option value="3" title="Ketu">Sapphire</option>
+              <option value="4" title="Ikoyi">Gold</option>
+              <option value="5" title="Oluyole, Ibadan">Silver hall</option>
+              <option value="6" title="Victoria Island Lagos">Ruby Hall</option>
+            </select>
+          </div>
 
-            <button type="submit" className="btn btn-success"><i className="fa fa-send"></i></button>
-          </form>
-      </div>
+          <div className="form-group">
+            <label htmlFor="date">Choose a date and time:<br />MM/DD/YYYY</label>
+            <input required className="form-control" type="date" name="date" id="date" value={date} onChange={handleChange} />
+          </div>
+
+          <button type="submit" className="btn btn-success"><i className="fa fa-send"></i></button>
+        </form>
+      </div></div>
     );
   }
 
 }
 
- export default EditEventForm;
+export default EditEventForm;
