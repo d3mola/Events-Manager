99c0444c398842dc36f6b035928e41f9514ad8ab
d3mola <ariyaademola@gmail.com>
2018-02-14 00:42:42 +0100

    Connect EventList component to redux store

--- client/components/EventList.jsx
+++ client/components/EventList.jsx
@@ -1,54 +1,76 @@
-import React from 'react'
-import PropTypes from 'prop-types';
-import EventItem from './EventItem.jsx';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import Header from './Header';
+import Footer from './Footer';
+import { Link } from "react-router-dom";
+import { getEvents } from "../actions/actionCreators";
+import EventItem from "./EventItem.jsx";
 
-// original implemenation using children props
-// no mapping here
-// const EventList = ({ title, children }) => (
-//   <div>
-//     <h3><strong><em>{title}</em></strong></h3>
-//     <div>{children}</div>
-//   </div>
-// )
+class EventList extends Component {
+  /**
+   * Creates an instance of EventList.
+   * @memberof Centers
+   * @param {object} props
+   */
+  constructor(props) {
+    super(props);
+  }
 
-// trial implementayion without children props
-// mapping here
-const EventList = ({ events, selectEvent, handleDeleteEvent }) => (
-  
-  !events ?
-  (
+  /**
+   *  get events as soon as component renders
+   * @returns {object} eventlist
+   * @memberof EventList
+   */
+  componentDidMount() {
+    this.props.getEvents();
+  }
+
+  render() {
+    const { events, match } = this.props;
+    return !events ? (
+      <div>
+        <em>No events...</em>
+        <Link to="/events/add">
+          <button className="btn btn-success">&#43;</button>
+        </Link>
+      </div>
+    ) : (
+      
     <div>
-      <em>No events...</em>
-      <Link to='/add-event'><button className='btn btn-success'>&#43;</button></Link>
+      <Header links={["centers", "events", "logout"]}/>
+      <div style={{fontWeight: 'bold'}}>
+        <h3>My Events</h3>
+        <Link to="/events/add">
+          <button className="btn btn-success btn-sm">
+            <i className="fa fa-plus" />
+          </button>
+        </Link>
+        <div>
+          {events.map(event => (
+            <div key={event.id}>
+              <EventItem
+                event={event}
+                match={match}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+      <Footer/>
     </div>
-  ) :
-  <div>
-    <h3>My Events</h3>
-    <Link to='/add-event'><button className='btn btn-success btn-sm'><i className="fa fa-plus"></i></button></Link>
-    {/* <Link to='/add-event'><button className='btn btn-success'>&#43;</button></Link> */}
-    <div>
-      {
-        events.map(event => (
-          <div key={event.id}>
-            <EventItem
-              // key={event.id}
-              event={event}
-              onCurrentEventClicked={() => selectEvent(event.id)}
-              handleDeleteEvent={() => handleDeleteEvent(event.id)}
-            />
+    );
+  }
+}
 
-            {/* alternative implementation of delete without going a component deeper */}
-            {/* <button onClick={() => handleDeleteEvent(event.id)}>X </button> */}
-          </div>
-        ))}
-    </div>
-  </div>
-)
+EventList.propTypes = {
+  events: PropTypes.array.isRequired,
+  getEvents: PropTypes.func.isRequired,
+  // title: PropTypes.string.isRequired
+}
 
-// EventList.propTypes = {
-//   children: PropTypes.node,
-//   title: PropTypes.string.isRequired
-// }
+const mapStateToProps = state => ({
+  events: state.eventsReducer.events
+});
 
-export default EventList
+export default connect(mapStateToProps, { getEvents })(EventList);
