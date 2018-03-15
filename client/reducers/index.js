b549aacbb7d74159ede1e1588d427e914e90c244
d3mola <ariyaademola@gmail.com>
2018-03-15 18:24:54 +0100

    Remove signup and signin reducer from index reducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,16 +1,18 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
-import signUp from './userSignUpReducer';
-import signIn from './signinReducer';
+// import signUp from './userSignUpReducer';
+// import signIn from './signinReducer';
 import centersReducer from './centersReducer';
 import eventsReducer from './eventsReducer';
 import flashMessages from './flashMessages';
+import authReducer from './authReducer';
 
 const rootReducer = combineReducers({
   flashMessages,
-  signUp,
-  signIn,
+  // signUp,
+  // signIn,
+  authReducer,
   centersReducer,
   eventsReducer,
   router: routerReducer
