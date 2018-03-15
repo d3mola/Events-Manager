ce400a050131146dfde03a1a06a331bcc1203203
d3mola <ariyaademola@gmail.com>
2018-03-15 18:20:47 +0100

    Refactor flashMessages reducer

--- client/reducers/flashMessages.js
+++ client/reducers/flashMessages.js
@@ -1,17 +1,21 @@
 import * as actionTypes from '../actions/actionTypes';
-import initialState from "./initialState";
+import initialState from './initialState';
 
-export default (state=initialState.flashMessages, action) => {
+export default (state = initialState.flashMessages, action) => {
   switch (action.type) {
     case actionTypes.FLASH_MESSAGE:
-      return action.payload;
+      return { ...state, payload: action.payload };
     case actionTypes.SUCCESS_FLASH_MESSAGE:
-      return {...state, message: action.response.message, className: 'alert-success'}
+      return {
+        ...state,
+        message: action.response.message,
+        className: 'alert-success'
+      };
     case actionTypes.FAILURE_FLASH_MESSAGE:
-      return {...state, message: action.error, className: 'alert-danger'}
+      return { ...state, error: action.error, className: 'alert-danger' };
     case actionTypes.CLEAR_FLASH_MESSAGE:
-      return {...state, message: null, className: null}
+      return { ...state, message: null, className: null, error: null };
     default:
       return state;
   }
-}
+};
