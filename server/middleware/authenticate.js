5f58480f88fbb38850074abd55b7ad479d6bb8d1
d3mola <ariyaademola@gmail.com>
2018-03-15 18:23:24 +0100

    Change error message when verifying token

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -11,15 +11,16 @@ dotenv.config();
  * @return {object} status code, token, error, message
  */
 const authenticate = (req, res, next) => {
-  const token = req.body.token || req.headers['x-access-token'] ||
-  (req.headers.Authorization && req.headers.Authorization.slice(7)) ||
-  req.params.token;
+  const token = req.body.token || req.headers['x-access-token'];
+  // ||
+  // (req.headers.Authorization && req.headers.Authorization.slice(7)) ||
+  // req.params.token;
 
   if (token) {
     jwt.verify(token, process.env.SECRET, (error, decoded) => {
       if (error) {
         return res.status(401).json({
-          message: 'token issues',
+          message: error.message,
           token
         });
       }
