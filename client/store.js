f02560c7a11084b137e79898f6ea67a18ab05a49
d3mola <ariyaademola@gmail.com>
2018-03-15 13:16:49 +0100

    Add redux-logger middleware

--- client/store.js
+++ client/store.js
@@ -1,20 +1,28 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+// import logger from 'redux-logger';
 import { routerMiddleware } from 'react-router-redux';
 
 import rootReducer from './reducers/index';
-import rootSaga from './Sagas/asyncOperations';
+import rootSaga from './Sagas/index';
 import history from './history';
 
 const sagaMiddleware = createSagaMiddleware();
 const routingMiddleware = routerMiddleware(history);
 
-const middleware = applyMiddleware(sagaMiddleware, routingMiddleware);
+const middleware = applyMiddleware(
+  sagaMiddleware,
+  routingMiddleware
+  // logger
+);
 
 // create store
 const store = createStore(
   rootReducer,
-  compose(middleware, window.devToolsExtension ? window.devToolsExtension() : f => f)
+  compose(
+    middleware,
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  )
 );
 
 sagaMiddleware.run(rootSaga);
