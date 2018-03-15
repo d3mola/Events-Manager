90c7230fe13d23e837b9836f4ee7dcd22a581150
d3mola <ariyaademola@gmail.com>
2018-03-15 18:35:30 +0100

    Clean up reducers

--- client/reducers/centersReducer.js
+++ client/reducers/centersReducer.js
@@ -8,30 +8,42 @@ import * as actionTypes from '../actions/actionTypes';
  */
 const centers = (state = initialState.centersReducer, action) => {
   switch (action.type) {
+    // case actionTypes.GET_CENTERS:
+    //   return { ...state, isFetching: true };
 
-    case 'GET_CENTERS_SUCCESS':
-      return {...state, centers: action.response.centers};
+    case actionTypes.GET_CENTERS_SUCCESS:
+      return { ...state, centers: action.centers, isFetching: false };
 
-    case 'ADD_CENTER_SUCCESS':
-      return {...state, center: action.response.center};
+    case actionTypes.GET_CENTERS_FAILURE:
+      return { ...state, isFetching: false };
 
-    case 'ADD_CENTER_FAILED':
+    case actionTypes.ADD_CENTER_SUCCESS:
+      return { ...state, center: action.center };
+
+    case actionTypes.ADD_CENTER_FAILURE:
       return state;
-      
+
     case actionTypes.GET_SINGLE_CENTER_SUCCESS:
-      return {...state, selectedCenter: action.response.center }
+      return { ...state, selectedCenter: action.response.center };
 
     case actionTypes.DELETE_CENTER_SUCCESS:
-      return {...state, deletedCenter: action.response.selectedCenter, message: action.response.message }
+      return {
+        ...state,
+        deletedCenter: action.response.selectedCenter,
+        message: action.response.message
+      };
 
     case actionTypes.DELETE_CENTER_FAILURE:
-      return {...state }
+      return { ...state };
 
     case actionTypes.EDIT_CENTER_SUCCESS:
-      return { ...state, updatedCenter: action.response.updatedCenter }
+      return { ...state, updatedCenter: action.response.updatedCenter };
 
     case actionTypes.EDIT_CENTER_FAILURE:
-      return {...state }
+      return { ...state };
+
+    case actionTypes.FETCHING:
+      return { ...state, fetchingCenters: action.status };
 
     default:
       return state;
@@ -39,4 +51,3 @@ const centers = (state = initialState.centersReducer, action) => {
 };
 
 export default centers;
-
