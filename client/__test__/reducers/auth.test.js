a6edd251b5c7796659a59dc3f814158939c5e4f6
Andela Developer <andeladeveloper@Andelas-MacBook-Pro-101.local>
2018-04-16 14:21:56 +0100

    Add more test cases for auth reducer

--- client/__test__/reducers/auth.test.js
+++ client/__test__/reducers/auth.test.js
@@ -8,42 +8,67 @@ const payload = { username: 'demola', password: 123456 };
 describe('Auth Reducers', () => {
   it('should return the initial state', () => {
     action = {};
-    newState = { isLoading: false, isAuthenticated: false };
-    expect(authReducer(undefined, action)).toEqual(newState);
+    expectedState = {
+      isLoading: false,
+      isAuthenticated: false,
+      token: null,
+      error: null
+    };
+    expect(authReducer(undefined, action)).toEqual(expectedState);
   });
   it('should handle SIGN_IN', () => {
     action = actions.signIn(payload);
     newState = authReducer(initialState.authReducer, action);
-    expectedState = { isLoading: true, isAuthenticated: false };
+    expectedState = {
+      isLoading: true,
+      isAuthenticated: false,
+      token: null,
+      error: null
+    };
     expect(newState).toEqual(expectedState);
   });
   it('should handle SIGN_IN_SUCCESS', () => {
+    localStorage.setItem('token', 'randomToken');
+    let token = localStorage.getItem('token');
     action = {
       type: types.SIGN_IN_SUCCESS,
-      response: { message: 'login successful' },
-      isAuthenticated: true
+      token
+      // response: { message: 'login successful' },
+      // isAuthenticated: true
     };
     newState = authReducer(initialState.authReducer, action);
-    expectedState = { isLoading: false, isAuthenticated: true };
-    expect(newState.isAuthenticated).toEqual(true);
+    // newState = authReducer(initialState.authReducer, types.SIGN_IN_SUCCESS);
+    // expectedState = { isLoading: false, isAuthenticated: true };
+    // expect(newState.isAuthenticated).toEqual(true);
+    // expect(newState.response).toEqual("login successful");
     expect(newState.isLoading).toEqual(false);
-    expect(newState.response).toEqual('login successful');
+    expect(newState.token).toEqual(localStorage.getItem('token'));
     expect(newState.error).toEqual(null);
   });
   it('should handle SIGN_IN_FAILURE', () => {
-    action = actions.loginFailure('login failed');
+    action = {
+      type: types.SIGN_IN_FAILURE,
+      error: 'login unsuccessful'
+    };
+    // const wrongPassword = {...payload, password: 12345678}
     newState = authReducer(initialState.authReducer, action);
     expectedState = {
       isLoading: false,
       isAuthenticated: false,
-      error: 'login failed'
+      token: null,
+      error: 'login unsuccessful'
     };
     expect(newState).toEqual(expectedState);
   });
   it('should handle LOG_OUT', () => {
-    action = { type: types.LOG_OUT_SUCCESS, isAuthenticated: false };
+    action = {
+      type: types.LOG_OUT_SUCCESS,
+      isAuthenticated: false,
+      token: null
+    };
     newState = authReducer(initialState.authReducer, action);
     expect(newState.isLoading).toEqual(false);
     expect(newState.isAuthenticated).toEqual(false);
+    expect(newState.token).toEqual(null);
   });
 });
