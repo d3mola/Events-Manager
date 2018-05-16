cb846ef6cb592690f289db92661d2834e418dae9
Ademola Ariya <ariyaademola@gmail.com>
2018-05-16 19:30:27 +0100

    [Feature #157644648] Fix heroku deployment issue
    - Change `start` script to use `node` from `babel-node` as babel-node is not optimized for production
    - Change `heroku-postbuild` script to run client and server builds instead of just running migrations which didnt do anything useful. Also removed the server build folder called `dist` before each build.
    - Move some dependecies to dependencies from dev dependencies because heroku does not read dev deps.
    - Use `cors` instead of the more verbose way of fixing cors issue
    - Serve static files from the build folder
    - Serve index.html from build
    - Add publicPath property to webpack.prod.js
    - Resolve .js and .jsx in webpack.prod.js
    - Add presets for `stage-2` and plugin for `transform-class-properties`
    - Use uglify in webpack prod config
    
    [Finishes #157644648]

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ import logger from 'morgan';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import path from 'path';
-// import cors from 'cors';
+import cors from 'cors';
 
 // import routes
 import routes from './routes/users';
@@ -17,46 +17,32 @@ dotenv.config();
 const app = express();
 const port = parseInt(process.env.PORT, 10) || 8000;
 
+// Using cors
+app.use(cors('*'));
+
 app.use(logger('dev'));
 
 // Parse incoming requests data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
-  // '*' is not good for production.
-  // Only if the API consumable is for public use.
-  //allow another domain use ur api.
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Methods',
-    'POST, GET, OPTIONS, PUT, DELETE, HEAD'
-  );
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, x-access-token'
-  );
-  next();
-});
-
-app.use('/', express.static(path.join(__dirname, 'client/static')));
-
+app.use('/', express.static(path.join(__dirname, 'client/build')));
 
 // use imported routes
 app.use('/api/v1', routes);
 app.use('/api/v1', centerRoutes);
 app.use('/api/v1', eventRoutes);
 
-// Always return the main index.html, so react-router renders
-// the route in the client
-app.get('/*', (req, res) => {
-  // res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
-  res.sendFile(path.join(path.dirname(__dirname), 'client/index.html'));
-});
-
 app.get('/bundle.js', (req, res) => res.sendFile(
   path.join(path.dirname(__dirname), 'client/build/bundle.js')
 ));
+
+// Always return the main index.html, so react-router renders
+// the route in the client
+app.get('/*', (req, res) => res.sendFile(
+  path.join(path.dirname(__dirname), 'client/build/index.html'))
+);
+
 
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
