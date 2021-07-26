be0a6b6952d6492c05252dacc202afe26bb37d0a
Damola12345 <ajiboladamola789@gmail.com>
2021-07-26 14:48:47 +0100

    terraform

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,8 +1,8 @@
 resource "aws_ecr_repository" "events" {
   name                 = "events"
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = ""
   image_scanning_configuration {
-    scan_on_push = true
+    scan_on_push = false
   }
 }
 
